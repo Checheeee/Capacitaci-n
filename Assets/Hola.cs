541ef761c3ccc8104e9ae15206b9e05c96899ec3
Francisco Goyeneche <f.goyeneche.camus@gmail.com>
2022-01-06 23:39:41 -0300

    Funcionalidad(Assets/Hola.cs): Mensaje salodo al iniciar juego

--- Assets/Hola.cs
+++ Assets/Hola.cs
@@ -7,7 +7,7 @@ public class Hola : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Holaaaaaaaa!");
     }
 
     // Update is called once per frame
